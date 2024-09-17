86ed97527342b83f0571c486a9d0d31d08bf1c19
Volodymyr Stefanyshyn <volodymyr.stefanyshyn@pandadoc.com>
2024-09-17 17:17:04 +0300

    ui skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+"use strict";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Logo />
+      <Search />
+      <Results />
+    </>
   );
 }
 
+function Logo() {
+  return (
+    <div className="background ">
+      <span className="logo">Giphy</span>
+    </div>
+  );
+}
+function Search() {
+  return (
+    <div className="searchContainer">
+      <input className="search" />
+      <button className="searchButton">Search</button>
+    </div>
+  );
+}
+function Results() {
+  return (
+    <div className="resultsContainer">
+      <div>Result 1</div>
+      <div>Result 2</div>
+      <div>Result 3</div>
+      <div>Result 4</div>
+      <div>Result 5</div>
+      <div>Result 6</div>
+      <div>Result 7</div>
+      <div>Result 8</div>
+      <div>Result 9</div>
+      <div>Result 10</div>
+      <div>Result 11</div>
+      <div>Result 12</div>
+      <div>Result 13</div>
+      <div>Result 14</div>
+    </div>
+  );
+}
 export default App;
