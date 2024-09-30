a41723625a73eb0275f8245c5944cc4f49df1aba
Volodymyr Stefanyshyn <volodymyr.stefanyshyn@pandadoc.com>
2024-09-30 14:11:48 +0300

    Search text removal after sumbitting the form

--- src/App.js
+++ src/App.js
@@ -2,44 +2,60 @@
 
 import { useState } from "react";
 import { useEffect } from "react";
+// import { searchGifUrl, trendingGifsUrl } from "./api";
 
 function App() {
   const [gifs, setGifs] = useState();
   const [isLoading, setIslonading] = useState(true);
   const [searchField, setSearchField] = useState("");
+
   const [searching, setSearching] = useState(false);
-  console.log(searchField);
-  function handleSetSearchfield() {
-    searchField = setSearchField;
+  console.log(searching);
+  const [searchInput, setSearchInput] = useState("");
+  console.log(searchInput);
+
+  const searchGifUrl = `https://api.giphy.com/v1/gifs/search?api_key=joJzQZk80ep6DF1ocKX2saSSNGU69GYg&q=${searchField}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`;
+  const trendingGifsUrl =
+    "https://api.giphy.com/v1/gifs/trending?api_key=joJzQZk80ep6DF1ocKX2saSSNGU69GYg&limit=25&offset=0&rating=g&bundle=messaging_non_clips";
+
+  async function handleOnClickSearch() {
+    try {
+      const searchedList = await fetch(searchGifUrl);
+      const searchedListData = await searchedList.json();
+
+      setGifs(searchedListData.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+
+    setSearching(true);
+    setSearchInput("");
+    console.log(gifs.length);
+  }
+  async function handleCloseSearch() {
+    try {
+      const trending = await fetch(trendingGifsUrl);
+      const trendingData = await trending.json();
+      setGifs(trendingData.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+    setSearching(false);
   }
 
-  function handleOnClickSearch() {
-    setSearching(!searching);
-  }
   useEffect(() => {
     const fetchGifs = async () => {
       try {
-        if (!searching) {
-          const trending = await fetch(
-            "https://api.giphy.com/v1/gifs/trending?api_key=joJzQZk80ep6DF1ocKX2saSSNGU69GYg&limit=25&offset=0&rating=g&bundle=messaging_non_clips"
-          );
-          const trendingData = await trending.json();
-          setGifs(trendingData.data);
-        } else if (searching) {
-          const searchedList = await fetch(
-            `https://api.giphy.com/v1/gifs/search?api_key=joJzQZk80ep6DF1ocKX2saSSNGU69GYg&q=${searchField}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`
-          );
-          const searchedListData = await searchedList.json();
-          setGifs(searchedListData.data);
-        }
-
+        const trending = await fetch(trendingGifsUrl);
+        const trendingData = await trending.json();
+        setGifs(trendingData.data);
         setIslonading(false);
       } catch (err) {
         console.log(err.message);
       }
     };
     fetchGifs();
-  }, [searching]);
+  }, []);
 
   if (isLoading) {
     return <p>Loading</p>;
@@ -49,10 +65,14 @@ function App() {
     <>
       <Logo />
       <Search
-        handleSetSearchfield={setSearchField}
         handleOnClickSearch={handleOnClickSearch}
+        handleCloseSearch={handleCloseSearch}
+        searching={searching}
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+        setSearchField={setSearchField}
       />
-      <ResultsList gifs={gifs} />
+      <ResultsList gifs={gifs} searchField={searchField} />
     </>
   );
 }
@@ -64,28 +84,55 @@ function Logo() {
     </div>
   );
 }
-function Search({ handleSetSearchfield, handleOnClickSearch }) {
+
+function Search({
+  handleOnClickSearch,
+  handleCloseSearch,
+  searching,
+  searchInput,
+  setSearchInput,
+  setSearchField,
+}) {
   return (
-    <div className="searchContainer">
-      <input
-        className="search"
-        onChange={(e) => handleSetSearchfield(e.target.value)}
-      />
-      <button className="searchButton" onClick={handleOnClickSearch}>
-        Search
-      </button>
-    </div>
+    <>
+      <div className="searchContainer">
+        <input
+          className="search"
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+            setSearchField(e.target.value);
+          }}
+        />
+        <button className="searchButton" onClick={() => handleOnClickSearch()}>
+          Search
+        </button>
+      </div>
+      {searching ? (
+        <div className="closeSearchContainer" onClick={handleCloseSearch}>
+          <button className="closeSearch crossButton">
+            Close search results X
+          </button>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 }
 
-function ResultsList({ gifs }) {
+function ResultsList({ gifs, searchField }) {
   return (
     <>
-      <div className="resultsContainer">
-        {gifs.map((gif) => (
-          <Result key={gif.id} gif={gif} />
-        ))}
-      </div>
+      {gifs.length ? (
+        <div className="resultsContainer">
+          {gifs.map((gif) => (
+            <Result key={gif.id} gif={gif} />
+          ))}
+        </div>
+      ) : (
+        <p>No results for {searchField}</p>
+      )}
     </>
   );
 }
@@ -93,25 +140,8 @@ function ResultsList({ gifs }) {
 function Result({ gif }) {
   return (
     <li>
-      <img src={gif.images.original.url} />
+      <img className="gifContainer" src={gif.images.original.url} />
     </li>
   );
 }
 export default App;
-
-// useEffect(() => {
-//   const fetchGifs = async () => {
-//     try {
-//       const trending = await fetch(
-//         "https://api.giphy.com/v1/gifs/trending?api_key=joJzQZk80ep6DF1ocKX2saSSNGU69GYg&limit=25&offset=0&rating=g&bundle=messaging_non_clips"
-//       );
-//       const trendingData = await trending.json();
-
-//       setGifs(trendingData.data);
-//       setIslonading(false);
-//     } catch (err) {
-//       console.log(err.message);
-//     }
-//   };
-//   fetchGifs();
-// }, []);
